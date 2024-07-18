48acd07e66f539c4dc496438f32ee9616493b853
ANKUSH THAKUR <ankukush05@gmail.com>
2024-07-18 11:57:39 +0530

    initiak commit

--- seed.js
+++ seed.js
@@ -1,26 +1,28 @@
-const mongoose = require("mongoose")
-const { products } = require("./data")
+const mongoose = require("mongoose");
+const { products } = require("./data");
 
-const Product = require("./models/productModel")
+const Product = require("./models/productModel");
 
-mongoose.connect(`mongodb://127.0.0.1:27017/organic`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    autoIndex: false
-}).then(() => {
-    console.log("connectdb")
-})
-
-
+mongoose
+  .connect(
+    `mongodb+srv://ankushiit21:WBwVG65PS1WmMWPA@cluster0.7gpdail.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`,
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      autoIndex: false,
+    }
+  )
+  .then(() => {
+    console.log("connectdb");
+  });
 
 const seedProduct = async () => {
-    try {
-        await Product.deleteMany()
-        await Product.insertMany(products)
-    } catch (error) {
-        console.log(error)
-    }
-}
+  try {
+    await Product.deleteMany();
+    await Product.insertMany(products);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-seedProduct()
-
+seedProduct();
